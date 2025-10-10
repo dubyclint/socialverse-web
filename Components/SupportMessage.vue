e4a939971ef5c05380095bd3c70be95b61d4ad97
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-10 15:39:57 +0100

    Create SupportMessage.vue

---
+++ Components/SupportMessage.vue
@@ -0,0 +1,12 @@
+ 
+<!-- components/SupportMessage.vue -->
+<template>
+  <div class="support-message">
+    <p>{{ displayedText }}</p>
+    <TranslationButton 
+      :original-text="message.content" 
+      :content-id="message.id"
+    />
+  </div>
+</template>
+ 
