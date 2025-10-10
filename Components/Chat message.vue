2a3e7934b48299650be23d3336aabe7d136ccfee
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-10 15:23:55 +0100

    Create Chat message.vue

---
+++ Components/Chat message.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div class="message">
+    <p>{{ displayedText }}</p>
+    <TranslationButton 
+      :original-text="message.content" 
+      :content-id="message.id"
+    />
+  </div>
+</template>
+ 
