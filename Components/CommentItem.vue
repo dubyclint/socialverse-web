92c4bfef6d60213095fdcf6073a555fb45ba36a6
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-10 15:20:09 +0100

    Create CommentItem.vue

---
+++ Components/CommentItem.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div class="comment">
+    <p>{{ displayedText }}</p>
+    <TranslationButton 
+      :original-text="comment.text" 
+      :content-id="comment.id"
+    />
+  </div>
+</template>
+ 
