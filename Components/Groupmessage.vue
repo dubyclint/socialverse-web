9e250d450f96a1796e9c406d5abb0d224e201e7b
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-10 15:26:51 +0100

    Create Groupmessage.vue

---
+++ Components/Groupmessage.vue
@@ -0,0 +1,12 @@
+ 
+<!-- components/GroupMessage.vue -->
+<template>
+  <div class="group-message">
+    <p>{{ displayedText }}</p>
+    <TranslationButton 
+      :original-text="message.text" 
+      :content-id="message.id"
+    />
+  </div>
+</template>
+ 
