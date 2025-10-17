be22b76c21cf8e56093bcd239ab3db1a1a7945ea
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-17 14:38:38 +0100

    Create StatusView model for tracking status views

---
+++ models/statusView.js
@@ -0,0 +1,40 @@
+// models/statusView.js
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../database/connection');
+
+const StatusView = sequelize.define('StatusView', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  statusId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'UserStatuses',
+      key: 'id'
+    }
+  },
+  viewerId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'id'
+    }
+  },
+  viewedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  timestamps: false,
+  indexes: [
+    { fields: ['statusId', 'viewerId'], unique: true },
+    { fields: ['statusId'] },
+    { fields: ['viewerId'] }
+  ]
+});
+
+module.exports = StatusView;
