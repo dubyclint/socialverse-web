0825377cf7931b8fa7bf40f29281773d1d1185c9
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-19 00:26:23 +0100

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -8,5 +8,11 @@ export { default as RankAdminPanel } from './RankAdminPanel.vue'
 export { default as TradeListings } from './TradeListings.vue'
 export { default as UserInbox } from './UserInbox.vue'
 export { default as VerifiedBadgeAdmin } from './VerifiedBadgeAdmin.vue'
+// Auto-import components
+export { default as ModernHeader } from './layout/ModernHeader.vue'
+export { default as ModernFooter } from './layout/ModernFooter.vue'
+export { default as MainFeed } from './feed/MainFeed.vue'
+export { default as PostCard } from './posts/PostCard.vue'
+export { default as Icon } from './ui/Icon.vue'
 
 // Remove any duplicate exports or conflicting imports
