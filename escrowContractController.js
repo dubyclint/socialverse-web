faf5a1cdcf2e46e2ed353d3f1d5de4011f057fc7
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-20 23:56:46 +0100

    Delete escrowContractController.js

--- escrowContractController.js
+++
@@ -1,7 +0,0 @@
-const io = req.app.get('io')
-io.emit('escrow:update', { dealId, action: 'released' }) // or 'refunded' or 'created'
-io.emit('escrow:userUpdate', {
-  dealId,
-  action: 'released', // or 'refunded', 'created'
-  userId: buyerId // or sellerId depending on context
-})
