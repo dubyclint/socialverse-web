b7d527c202f8e02278482dbbd19c40afcbaf6605
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-20 23:08:26 +0100

    Delete escrowRoutes.js

--- escrowRoutes.js
+++
@@ -1,7 +0,0 @@
-// server/routes/escrowRoutes.js
-const express = require('express')
-const router = express.Router()
-const { createEscrow } = require('../controllers/escrowController')
-
-router.post('/create', createEscrow)
-module.exports = router
