a58546f0197245d989b9ab1efbc3646915ac9a5b
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-20 23:34:37 +0100

    Delete pewRoutes.js

--- pewRoutes.js
+++
@@ -1,8 +0,0 @@
-// server/routes/pewRoutes.js
-const express = require('express')
-const router = express.Router()
-const { createPew } = require('../controllers/pewController')
-
-router.post('/create', createPew)
-
-module.exports = router
