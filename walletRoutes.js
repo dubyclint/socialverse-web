562cdd59833185a2bf562004389a3ef5c5338a5e
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-20 23:53:31 +0100

    Delete walletRoutes.js

--- walletRoutes.js
+++
@@ -1,8 +0,0 @@
-// server/routes/walletRoutes.js
-const express = require('express')
-const router = express.Router()
-const { createWallets } = require('../controllers/walletController')
-
-router.post('/wallets/create', createWallets)
-
-module.exports = router
