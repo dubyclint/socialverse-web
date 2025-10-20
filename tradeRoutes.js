5eca19a9ace15d0f320c3c85dfb430845e20d7b0
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-20 23:47:32 +0100

    Delete tradeRoutes.js

--- tradeRoutes.js
+++
@@ -1,7 +0,0 @@
-// server/routes/tradeRoutes.js
-const express = require('express')
-const router = express.Router()
-const { createTrade } = require('../controllers/tradeController')
-
-router.post('/create', createTrade)
-module.exports = router
