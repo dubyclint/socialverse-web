6a670105ae5a7df6ea8bc43e88b91844d26bd2b4
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-22 03:32:50 +0100

    Delete matchRoutes.js

--- matchRoutes.js
+++
@@ -1,7 +0,0 @@
-// server/routes/matchRoutes.js
-const express = require('express')
-const router = express.Router()
-const { submitRequest } = require('../controllers/matchController')
-
-router.post('/submit', submitRequest)
-module.exports = router
