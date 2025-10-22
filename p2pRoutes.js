19050e5d7ad4e2cd30f19283151b48fdb1a52191
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-22 03:28:18 +0100

    Delete p2pRoutes.js

--- p2pRoutes.js
+++
@@ -1,7 +0,0 @@
-// server/routes/p2pRoutes.js
-const express = require('express')
-const router = express.Router()
-const { submitProfile } = require('../controllers/p2pController')
-
-router.post('/submit', submitProfile)
-module.exports = router
