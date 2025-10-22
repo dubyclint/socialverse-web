b3344d390cfe579f0fd1d0578140670e97bd9ed0
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-22 03:32:00 +0100

    Delete notificationRoutes.js

--- notificationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getNotifications, markRead } = require('../controllers/notificationController')
-
-router.get('/list', getNotifications)
-router.post('/read', markRead)
-
-module.exports = router
