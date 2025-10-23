49c194984616c6cc7805c307e097a91d74bca3cf
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-23 09:05:12 +0100

    Create sitemap.xml.ts

---
+++ server/routes/sitemap.xml.ts
@@ -0,0 +1,24 @@
+export default defineEventHandler((event) => {
+  setHeader(event, 'Content-Type', 'application/xml')
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://socialverse-web.zeabur.app</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://socialverse-web.zeabur.app/explore</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://socialverse-web.zeabur.app/feed</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>`
+})
