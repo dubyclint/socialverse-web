e592565f634c8d1bf260a33c827225d91a2c9976
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-10-27 11:29:16 +0100

    Update gun-client.ts

--- plugins/gun-client.ts
+++ plugins/gun-client.ts
@@ -2,7 +2,25 @@ import Gun from 'gun/gun'
 import 'gun/sea'
 
 // Initialize Gun instance with peer server
-const gunInstance = Gun(['https://gun-messaging-peer.herokuapp.com/gun'])
+// Wrapped in try-catch to prevent app breaking if Gun fails
+let gunInstance: any = null
+
+try {
+  gunInstance = Gun({
+    peers: ['https://gun-messaging-peer.herokuapp.com/gun'],
+    localStorage: false, // Disable localStorage to avoid issues
+    radisk: false // Disable radisk
+  })
+  console.log('Gun: Initialized successfully')
+} catch (err) {
+  console.error('Gun: Initialization failed:', err)
+  // Create a dummy Gun instance that doesn't do anything
+  gunInstance = {
+    get: () => ({ on: () => {}, once: () => {} }),
+    put: () => ({ on: () => {}, once: () => {} }),
+    set: () => ({ on: () => {}, once: () => {} }),
+  }
+}
 
 // Export a function to get the Gun instance
 export const useGun = () => {
