7192b5f5b97a3d93cb71820a7c14285a96d53382
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-05 05:16:56 +0100

    Update useSupabase.js

--- composables/useSupabase.js
+++ composables/useSupabase.js
@@ -1,4 +1,19 @@
-// composables/useSupabase.js
+// FILE: /composables/useSupabase.js - FIXED VERSION
+// This composable wraps useSupabaseClient from @nuxtjs/supabase
+// It should only be used on the client side
+
 export const useSupabase = () => {
-  return useSupabaseClient()
+  // Check if we're on the server side
+  if (process.server) {
+    console.warn('[useSupabase] This composable should only be used on the client side')
+    return null
+  }
+  
+  // This will work on the client side because @nuxtjs/supabase auto-imports it
+  try {
+    return useSupabaseClient()
+  } catch (error) {
+    console.error('[useSupabase] Error getting Supabase client:', error)
+    return null
+  }
 }
