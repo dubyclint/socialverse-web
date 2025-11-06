7934601b54a021122a0e5a47fae494c5dfe2388e
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-06 04:42:49 +0100

    Create login.vue

---
+++ pages/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+// Redirect to /auth/login
+definePageMeta({
+  layout: false,
+})
+
+const router = useRouter()
+
+onMounted(() => {
+  router.push('/auth/login')
+})
+</script>
