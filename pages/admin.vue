7099ef19965bf0a2a4f4b04954048338f5ffce7b
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-11 22:07:49 +0100

    Update admin.vue

--- pages/admin.vue
+++ pages/admin.vue
@@ -9,9 +9,9 @@
 </template>
 
 <script setup>
-import adminuserlist from '@/components/admin-user-list.vue'
-import adminpostmoderation from '@/components/admin-post-moderation.vue'
-import adminmatchapproval from '@/components/admin-match-approval.vue'
-import adminescrowcontrol from '@/components/admin-escrow-control.vue'
-import adminfeeManager from '@/components/admin-fee-manager.vue'
+import adminuserlist from '@/components/admin/user-list.vue'
+import adminpostmoderation from '@/components/admin/post-moderation.vue'
+import adminmatchapproval from '@/components/admin/match-approval.vue'
+import adminescrowcontrol from '@/components/admin/escrow-control.vue'
+import adminfeeManager from '@/components/admin/fee-manager.vue'
 </script>
