dafaaed1bdd62cf68f1a69266b61ca1830543ff7
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-11 23:33:50 +0100

    Update toast.client.ts

--- plugins/toast.client.ts
+++ plugins/toast.client.ts
@@ -1,6 +1,7 @@
-import Toast from 'vue-toastification'
-import 'vue-toastification/dist/index.css'
+// REMOVED: Vue 2 incompatible vue-toastification
+// For Vue 3 Nuxt, use native notifications or Nuxt UI instead
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(Toast)
+  // Toast notifications disabled - vue-toastification is Vue 2 only
+  // Consider using: @nuxtui/ui, sonner, or native browser notifications
 })
