13af8a58a0d5e114b02c9ad78988724a7ecc446b
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-12 01:14:55 +0100

    Update posts.vue

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <CreatePost />
+    <createpost />
     <h2>Latest Posts</h2>
     <PostCard v-for="post in posts" :key="post.id" :post="post" />
     <h2>Top Pew</h2>
@@ -10,9 +10,9 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import CreatePost from '@/components/posts/create-post.vue'
-import PostCard from '@/components/posts/post-card.vue'
-import PewCard from '@/components/pew-card.vue'
+import createpost from '@/components/posts/create-post.vue'
+import postcard from '@/components/posts/post-card.vue'
+import pewcard from '@/components/pew-card.vue'
 
 const posts = ref([])
 const pews = ref([])
