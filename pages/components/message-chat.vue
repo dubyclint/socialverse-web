059bb0c20ed1b4840447f16ac59076720e64d41b
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-12 01:16:29 +0100

    Update message-chat.vue

--- pages/components/message-chat.vue
+++ pages/components/message-chat.vue
@@ -7,6 +7,6 @@
 </template>
 
 <script setup>
-import VerifiedBadgeIcon from '@/components/VerifiedBadgeIcon.vue'
+import verifiedbadgeicon from '@/components/verified-badge-icon.vue'
 defineProps({ message: Object })
 </script>
