425151b48e207bff8e23329e89a393fd9d43cd8c
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-12 01:12:15 +0100

    Update my-pocket.vue

--- pages/my-pocket.vue
+++ pages/my-pocket.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <WalletOverview />
-    <CryptoSwap />
+    <walletoverview />
+    <cryptoswap />
   </div>
 </template>
 
 <script setup>
-import WalletOverview from '@/components/WalletOverview.vue'
-import CryptoSwap from '@/components/CryptoSwap.vue'
+import walletoverview from '@/components/wallet-overview.vue'
+import cryptoswap from '@/components/crypto-swap.vue'
 </script>
