ee994317ae6f43d31ce277d19b5f4ec4413f3b3f
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-14 01:27:29 +0100

    Update escrow-form.vue

--- components/escrow-form.vue
+++ components/escrow-form.vue
@@ -16,12 +16,12 @@
 
 <script setup>
 import { ref } from 'vue'
-import { useEscrowContract } from '@/composables/useEscrowContract'
+import { useEscrowContract } from '@/composables/use-escrow-contract'
 
 const seller = ref('')
 const amount = ref(0)
 const txHash = ref('')
-const { createDeal } = useEscrowContract()
+const { createDeal } = useescrowcontract()
 
 async function submitDeal() {
   txHash.value = await createDeal(seller.value, BigInt(amount.value * 1e6))
