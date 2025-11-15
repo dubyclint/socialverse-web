0dfb71b6a227510696987da764523929f036e44a
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-15 05:32:33 +0100

    Update pewgift-controller.js

--- server/api/controllers/pewgift-controller.js
+++ server/api/controllers/pewgift-controller.js
@@ -1,5 +1,5 @@
 // server/api/controllers/giftController.js
-import { supabase } from '../../../utils/supabase.js';
+import { supabase } from '../../utils/supabase.js';
 
 export class GiftController {
   // Send gift
