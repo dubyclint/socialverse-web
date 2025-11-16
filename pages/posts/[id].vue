468dab793924536a56c93eb429f787be45182d67
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-16 22:30:26 +0100

    Update [id].vue

--- pages/posts/[id].vue
+++ pages/posts/[id].vue
@@ -7,10 +7,15 @@
   </div>
 </template>
 
-<script setup>
+<script setup><script setup lang="ts">
+definePageMeta({
+  middleware: ['auth', 'language-check'],
+  layout: 'default'
+})
+ 
 import { useRoute } from 'vue-router';
-// import PostDetail from '~/components/PostDetail.vue'; // Component may not exist
-// import AdSlot from '~/components/AdSlot.vue'; // Component may not exist
+import PostDetail from '~/components/post-detail.vue'; // Component may not exist
+import AdSlot from '~/components/ad-slot.vue'; // Component may not exist
 
 const route = useRoute();
 const id = route.params.id;
