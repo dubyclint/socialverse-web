24dc497b40b5da926e247f94b0bef31f8ac3f74b
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-17 00:24:40 +0100

    Update admin-fee-dashboard.vue

--- pages/admin-fee-dashboard.vue
+++ pages/admin-fee-dashboard.vue
@@ -43,7 +43,12 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['route-guard', 'language-check', 'security-middleware'],
+  layout: 'default'
+})
+ 
 import { ref, onMounted } from 'vue'
 
 const fee = ref({
