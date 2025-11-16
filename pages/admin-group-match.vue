1eb484287a4f4268cbfaaedfa88a74f3987c86a2
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-17 00:28:00 +0100

    Update admin-group-match.vue

--- pages/admin-group-match.vue
+++ pages/admin-group-match.vue
@@ -22,7 +22,12 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['route-guard', 'language-check', 'security-middleware'],
+  layout: 'default'
+})
+ 
 import { ref } from 'vue'
 
 const overrideGroup = ref('')
