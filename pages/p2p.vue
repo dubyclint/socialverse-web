a1e63210dbb6189a6e68a09752dbc67d6b4c9be6
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-16 20:09:11 +0100

    Update p2p.vue

--- pages/p2p.vue
+++ pages/p2p.vue
@@ -6,7 +6,12 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['auth', 'language-check'],
+  layout: 'default'
+})
+ 
 import p2ptradeform from '@/components/p2p-trade-form.vue'
 import tradechat from '@/components/trade-chat.vue'
 import escrowstatus from '@/components/escrow-status.vue'
