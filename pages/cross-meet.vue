32f38b55ea8f755545c59608e869d3174888d557
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-16 20:05:02 +0100

    Update cross-meet.vue

--- pages/cross-meet.vue
+++ pages/cross-meet.vue
@@ -5,7 +5,12 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['auth', 'language-check'],
+  layout: 'default'
+})
+  
 import matchrequestform from '@/components/match-request-form.vue'
 import matchresults from '@/components/match-results.vue'
 </script>
