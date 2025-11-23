ac5d350950f653814114a4bf511c67f6161613ad
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-23 13:11:40 +0100

    Update use-gun.js

--- use-gun.js
+++ use-gun.js
@@ -1,13 +1,19 @@
+// use-gun.ts
 import Gun from 'gun/gun'
 import 'gun/sea'
 
-// Connect to your local Nuxt server's Gun peer
-const gun = Gun({
+interface GunInstance {
+  get: (key: string) => any
+  put: (data: any) => any
+  on: (callback: (data: any) => void) => any
+}
+
+const gun: GunInstance = Gun({
   peers: [
-    typeof window !== 'undefined' && window.location.origin 
+    typeof window !== 'undefined' && window.location.origin
       ? `${window.location.origin}/gun`
       : 'http://localhost:3000/gun'
-  ],
+  ]
 })
 
 export default gun
