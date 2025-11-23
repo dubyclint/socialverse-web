24058f7ba7de96ad07f3c3e1041475d22cc0c1ea
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-23 14:42:59 +0100

    Delete server/controllers/user-wallet-controller.js

--- server/controllers/user-wallet-controller.js
+++
@@ -1,8 +0,0 @@
-const UserWallet = require('../models/UserWallet')
-
-exports.getWallet = async (req, res) => {
-  const { userId } = req.query
-  const wallet = await UserWallet.findOne({ userId })
-  res.status(200).json(wallet)
-}
-
