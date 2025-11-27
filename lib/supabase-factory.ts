449b28ab11e7408dd78e1555d02f41d67fe33418
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-11-27 21:57:25 +0100

    Update supabase-factory.ts

--- lib/supabase-factory.ts
+++ lib/supabase-factory.ts
@@ -7,9 +7,8 @@ let supabaseClient: any = null
 
 export const getSupabaseClient = () => {
   if (!supabaseClient) {
-    const config = useRuntimeConfig()
-    const supabaseUrl = config.public.supabaseUrl
-    const supabaseKey = config.public.supabaseKey
+    const supabaseUrl = 'https://cvzrhucbvezqwbesthek.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNenJodWNidmVcXdiZXN0aGVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzNzgzMjYsImV4cCI6MjA3NDkNDMyNnk5QEwTbECqNxwt_HaUjUGDlYsHWuPrQVjY4I'
 
     if (!supabaseUrl || !supabaseKey) {
       console.error('[Supabase Factory] Missing Supabase credentials')
