416389ee2729bb89fa704dc9d030e7128ee36814
dubyclint <118350883+dubyclint@users.noreply.github.com>
2025-12-10 03:29:35 +0100

    Update error-handler.ts

--- server/error-handler.ts
+++ server/error-handler.ts
@@ -1,8 +1,5 @@
 // server/error-handler.ts
 export default defineEventHandler((event) => {
-  // This is a global error handler for Nitro
-  // It will catch any unhandled errors during request processing
-  
   const error = event.node.req.error
   
   if (error) {
