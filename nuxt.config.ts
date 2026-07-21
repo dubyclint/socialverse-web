e7437ba594e894fa3f3c16fe40f4c18984547548
dubyclint <118350883+dubyclint@users.noreply.github.com>
2026-07-21 03:40:16 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,8 +34,8 @@ export default defineNuxtConfig({
   ],
 
   supabase: {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_ANON_KEY,
+    url: process.env.SUPABASE_URL || process.env.NUXT_PUBLIC_SUPABASE_URL,
+    key: process.env.SUPABASE_ANON_KEY || process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY,
     redirect: true,
     redirectOptions: {
       login: '/signin',
